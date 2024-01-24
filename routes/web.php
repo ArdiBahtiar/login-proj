f91b6403bd47f1c6d61a2b5e81c94e435bbcf2b0
ArdiBahtiar <muhammadardibachtiar@gmail.com>
2024-01-24 14:43:51 +0700

    pulling from main

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,8 @@ use Illuminate\Auth\Events\Login;
 //     return view('welcome');
 // });
 
-Route::get('/', [LoginController::class, 'login'])->name('login');      
-Route::post('actionlogin', [LoginController::class, 'actionlogin'])->name('actionlogin');       
+Route::get('/', [LoginController::class, 'login'])->name('login');                                  // NAMED ROUTES, untuk ngasih nama dari suatu Route
+Route::post('actionlogin', [LoginController::class, 'actionlogin'])->name('actionlogin');           // 
 
 Route::get('home', [HomeController::class, 'index'])->name('home')->middleware('auth');
 Route::get('actionlogout', [LoginController::class, 'actionlogout'])->name('actionlogout')->middleware('auth');
